de4ecc1c989bba011f910219a8de7573582fcbc2
Stefan Meinecke <meinecke@greensec.de>
2025-02-22 19:46:24 +0100

    fix syntax.

--- bot.py
+++ bot.py
@@ -11,4 +11,6 @@ if __name__ == "__main__":
     if app["bot_manager"].config.get("http", {}).get("socket"):
         web.run_app(app, path=app["bot_manager"].config["http"]["socket"])
     else:
-        web.run_app(app, host=app["bot_manager"].config.get("http", {}).get("host", "127.0.0.1"), port=app["bot_manager"].config.get("http", {}).get("port", 8080))
+        web.run_app(
+            app, host=app["bot_manager"].config.get("http", {}).get("host", "127.0.0.1"), port=app["bot_manager"].config.get("http", {}).get("port", 8080)
+        )
